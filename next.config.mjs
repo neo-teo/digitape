e7f606978e8316f4fcb025775768825766f8f217
teo-clone <theodore.tsivranidis@gmail.com>
2024-04-16 17:38:19 -0400

    one more try

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,7 @@ const nextConfig = {
                 destination:
                     process.env.NODE_ENV === 'development'
                         ? 'http://127.0.0.1:3000/:path*'
-                        : '/api/:path*',
+                        : '/api/',
             },
         ]
     },
